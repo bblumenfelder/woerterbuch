3f96c8d9da048817114d22d5e786eebb66154093
bblumenfelder <gingerfish.bene@gmx.de>
2018-03-28 23:04:06 +0200

    show search results

--- Woerterbuch.vue
+++ Woerterbuch.vue
@@ -1,24 +1,26 @@
 <template>
-    <div>
+    <div class="woerterbuch">
         <div class="woerterbuch__container">
             <label class="woerterbuch__suche--label">Suche lateinisches Wort oder Bedeutung </label>
             <div class="woerterbuch__suche--container flex_container_space_around">
                 <!--SUCHFELD-->
                 <input class="woerterbuch__suche--input input_text"
                        name="woerterbuch__suche--input"
-                       v-model="suchbegriff">
+                       placeholder="Lateinisches Wort oder Bedeutung eingeben..."
+                       v-model="search">
 
                 <!--RADIO-GROUP SUCHART-->
                 <div class="woerterbuch__suche--radio-group radio_group">
                     <div class="woerterbuch__suche--radio-container radio">
                         <label class='woerterbuch__suche--radio-label radio_element'>
-                            <input class="woerterbuch__suche--radio__schnellsuche" type="radio" name="suche_art" value="schnellsuche" checked>
+                            <input class="woerterbuch__suche--radio__schnellsuche" type="radio" name="suche_art" value="schnellsuche"
+                                   checked>
                             <span class="woerterbuch__suche--radio-title radio_label"> Schnellsuche</span>
                         </label>
                     </div>
                     <div class="woerterbuch__suche--radio-container radio">
                         <label class='woerterbuch__suche--radio-label radio_element'>
-                            <input  class="woerterbuch__suche--radio__volltextsuche" type="radio" name="suche_art" value="volltextsuche">
+                            <input class="woerterbuch__suche--radio__volltextsuche" type="radio" name="suche_art" value="volltextsuche">
                             <span class="woerterbuch__suche--radio-title radio_label"> Volltextsuche</span>
                         </label>
                     </div>
@@ -26,11 +28,9 @@
             </div>
         </div>
 
-        <!--LOADER-->
-        <vue-loading-dots v-if="results"></vue-loading-dots>
 
         <!--ERGEBNISSE-->
-        <div class="woerterbuch__ergebnisse_container">
+        <div v-if="hasResults()" class="woerterbuch__ergebnisse_container">
             <div class="woerterbuch__ergebnisse__header">
                 <label>Ergebnisse: </label>
 
@@ -43,18 +43,17 @@
             <div class="woerterbuch__ergebnisse__body--container">
                 <div class="woerterbuch__ergebnisse__body--container-sub">
                     <a
-                       class="woerterbuch__ergebnisse__body--result-btn"
-                       v-for="wort in results"
-                       href="#"
-                       @click.prevent="viewVocab(wort)"
-                       v-bind:vocab_id="wort.id"
-                       v-bind:route_name="wort.route_name">
+                            class="woerterbuch__ergebnisse__body--result-btn js-opens-modal"
+                            v-for="wort in results"
+                            href="#"
+                            @click.prevent="viewVocab(wort)">
 
                         <div class="woerterbuch__ergebnisse__body--result-row">
-                            <span class="woerterbuch__ergebnisse__body--result-lemma" v-text="wort.lemma"></span>
+                            <span class="woerterbuch__ergebnisse__body--result-lemma lemma" v-text="wort.lemma"></span>
                             <span class="woerterbuch__ergebnisse__body--result-wortart" v-text="wort.wortart"></span>
                             <span class="woerterbuch__ergebnisse__body--result-bedeutung" v-text="wort.bedeutung"></span>
                         </div>
+
 
                     </a>
                 </div>
@@ -65,119 +64,297 @@
 
             </div>
         </div>
+        <!--LOADER-->
+        <vue-loading-dots v-if="isSearching()"></vue-loading-dots>
+
     </div>
 </template>
 
 <script>
+
+    import {VueLoading} from "../loader/hermeneus.vue_loading";
+
     export default {
-        name: "woerterbuch"
+        name: "woerterbuch",
+        data: function () {
+            return {
+                search: '',
+            };
+        },
+        components: {
+            'vue-loading-dots': VueLoading
+        },
+        props: ['vocab'],
+        computed: {
+            /**
+             *
+             */
+            results () {
+                return this.vocab.filter(wort => {
+                    let string_match = new RegExp(this.search, 'gi');
+                    return wort.lemma.match(string_match) !== null || wort.bedeutung.match(string_match) !== null;
+                });
+            }
+        },
+        methods: {
+            /**
+             *
+             */
+            isSearching () {
+            },
+            /**
+             *
+             * @returns {boolean}
+             */
+            hasResults () {
+                return this.search !== '' && this.results.length > 0;
+            },
+            /**
+             *
+             * @param wort
+             */
+            viewVocab (wort) {
+                console.log(wort);
+                this.openModal(wort);
+            },
+            /**
+             *
+             * @param wort
+             */
+            openModal(wort) {
+                let ModalData = {
+                  title: 'Vokabelansicht: ' + wort.lemma,
+                  type: 'success',
+                  size: 'fullscreen',
+                    data: 'hohoho'
+/*
+                  uri: '/glossarium/' + wort.route_name + '/' + wort.id,
+*/
+                };
+                EventBus.$emit('OpenModalEvent', JSON.stringify(ModalData));
+            }
+        },
+
     }
 </script>
 
 <style scoped>
-.suche-view{
-    padding: 0.75rem;
-}
+    /**
+     *  SUCHKOMPONENTEN
+     */
+    #ui_selected_vocab .selected_vocab_id {
+        display: inline-block;
+        color: #fff;
+        background: #B22E2F;
+        padding: 0.25rem 0.5rem;
+        margin: 0.25rem;
+    }
 
-.suche_container {
-    padding: 0.5rem;
-    min-height: 10rem;
-    text-align: center;
-}
+    #ui_selected_vocab .selected_vocab_id .fa {
+        color: #fff;
+    }
 
-.suche_container label {
-    display: block;
-    margin-bottom: 1rem;
-    font-size: 1.1rem;
-}
+    .woerterbuch {
+        background: #fff;
+    }
 
-.radio_group {
-    text-align: left;
-    width: 15%;
-    min-width: 10rem;
-}
+    .woerterbuch {
+        padding: 0.75rem;
+    }
 
-.radio {
-    display: inline-block;
-    margin-left: 0;
-}
+    .woerterbuch .woerterbuch__container {
+        padding: 0.5rem;
+        min-height: 10rem;
+        text-align: center;
+    }
 
-.radio label {
-    font-size: 1rem;
-    margin-bottom: 0;
-}
+    .woerterbuch__suche--label {
+        display: block;
+        margin-bottom: 1rem;
+        font-size: 1.1rem;
+        text-align: center;
+    }
 
-.suche_switch {
-    display: flex;
-    justify-content: space-between;
-}
+    .woerterbuch__suche--radio-group {
+        display: none;
+        text-align: left;
+        width: 15%;
+        min-width: 10rem;
+    }
 
-.suche_switch_span:before {
-    content: "Schnellsuche";
-}
+    .woerterbuch__suche--radio-container.radio {
+        display: inline-block;
+        margin-left: 0;
+    }
 
-.suche_switch_span:before input:checked + .suche_switch_span:before {
-    content: "Volltextsuche";
-}
-
-.suche_input_container {
-    margin: auto;
-}
-
-.ipt_search_vocab {
-    max-width: 75%;
-    min-width: 15rem;
-    margin-bottom: 1rem;
-    height: 3rem;
-    background: #eee;
-    box-shadow: inset 0 2px 5px 0 #ddd;
-    border: 1px solid #ddd;
-}
-
-.label_switch {
-    width: 20%;
-    height: 3rem;
-}
-
-.input_switch {
-    background: #eee;
-    box-shadow: inset 0 2px 5px 0 #ddd;
-    border: 1px solid #ddd;
-}
-
-.input_switch:before {
-    font-size: 0.8rem;
-    overflow: hidden;
-    color: #fff;
-    background: #B22E2F;
-    height: calc(2rem - 1px);
-    width: 7rem;
-    text-align: center;
-    padding-top: 0.25rem;
-    left: 0.5rem;
-    bottom: 0.5rem;
-}
-
-.input_switch:before input:checked + .input_switch:before {
-    -webkit-transform: translateX(calc(100% - 1rem));
-    -ms-transform: translateX(calc(100% - 1rem));
-    transform: translateX(calc(100% - 1rem));
-}
-
-.vocab_search_head {
-    color: #B22E2F;
-    padding: 0.5rem;
-}
-
-.vocab_search_head span {
-    text-align: center;
-}
-
-.results_header {
-    text-align: left;
-}
+    .woerterbuch__suche--radio-label {
+        font-size: 1rem;
+        margin-bottom: 0;
+    }
 
 
+    .woerterbuch__suche--container {
+        margin: auto;
+    }
+
+    .woerterbuch__suche--input {
+        /*max-width: 75%;*/
+        min-width: 15rem;
+        margin-bottom: 1rem;
+        height: 3rem;
+        background: #eee;
+        box-shadow: inset 0 2px 5px 0 #ddd;
+        border: 1px solid #ddd;
+    }
+
+
+    .woerterbuch__suche--radio-container input:checked + input:before {
+        -webkit-transform: translateX(calc(100% - 1rem));
+        -ms-transform: translateX(calc(100% - 1rem));
+        transform: translateX(calc(100% - 1rem));
+    }
+
+    .woerterbuch__ergebnisse_container {
+        padding: 0.5rem;
+    }
+
+    .woerterbuch__ergebnisse__header {
+        text-align: left;
+    }
+
+    .woerterbuch__ergebnisse__header--column-titles {
+        color: #B22E2F;
+        padding: 0.5rem;
+    }
+
+    .woerterbuch__ergebnisse__header--column-titles span {
+        text-align: center;
+    }
+
+    .woerterbuch__ergebnisse_container .woerterbuch__ergebnisse__header {
+          }
+
+    .woerterbuch__ergebnisse_container a {
+        text-decoration: none;
+    }
+
+    .woerterbuch__ergebnisse_container a:hover {
+        color: #B22E2F;
+    }
+
+    .woerterbuch__ergebnisse__body--container {
+        max-height: 25rem;
+        overflow-y: scroll;
+        overflow-x: hidden;
+        /*   display: none;*/
+        animation-name: fadeIn;
+        animation-duration: 0.4s;
+    }
+
+    @media only screen and (min-width: 640px) {
+        .woerterbuch {
+            padding: 4rem;
+        }
+
+        .woerterbuch__container {
+            padding: 2rem;
+        }
+
+        #result_container {
+            padding: 2rem;
+        }
+
+        .woerterbuch__ergebnisse__header--column-titles, .woerterbuch__ergebnisse__body--container-sub {
+            /*
+            display: flex;
+            justify-content: space-between;
+            */
+            text-align: left;
+            padding: 0.5rem;
+        }
+
+        .woerterbuch__ergebnisse__body--result-lemma, .woerterbuch__ergebnisse__header--column-titles__lemma {
+            width: 30%;
+        }
+
+        .woerterbuch__ergebnisse__header--column-titles__wortart, .woerterbuch__ergebnisse__body--result-wortart {
+            width: 10%;
+            display: inline-block;
+        }
+
+        .woerterbuch__ergebnisse__header--column-titles__bedeutung,
+        .woerterbuch__ergebnisse__body--result-bedeutung {
+            width: 40%;
+        }
+    }
+
+    .woerterbuch__ergebnisse__header--column-titles, .woerterbuch__ergebnisse__body--result-row {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        overflow: hidden;
+        text-align: left;
+        padding: 0.25rem;
+    }
+
+    .woerterbuch__ergebnisse__body--container {
+        border: 1px solid #eee;
+    }
+
+    .woerterbuch__ergebnisse__body--container a {
+        display: block;
+    }
+
+    .woerterbuch__ergebnisse__body--container a:nth-child(odd) {
+        background: #fff;
+    }
+
+    .woerterbuch__ergebnisse__body--container a:nth-child(even) {
+        background: #eee;
+    }
+
+    .woerterbuch__ergebnisse__body--container-sub {
+        line-height: 1rem;
+        padding: 0.25rem;
+        margin: 0 auto 0 auto;
+    }
+
+    .woerterbuch__ergebnisse__body--result-row:hover span {
+        color: #B22E2F;
+    }
+
+
+    .woerterbuch__ergebnisse__body--result-bedeutung {
+        display: inline-block;
+        font-size: 0.8rem;
+        line-height: 1rem;
+        color: #000;
+    }
+
+    .woerterbuch__ergebnisse__body--result-wortart {
+        color: #888;
+        font-size: 0.8rem;
+    }
+
+    /*    .woerterbuch__ergebnisse__body--result-wortart:hover {
+            color: #fff;
+        }*/
+
+    .woerterbuch__ergebnisse__body--container {
+        color: #fff;
+    }
+
+    .woerterbuch__suche--input {
+        width: 100%;
+        box-shadow: 0px 1px 3px #ddd;
+    }
+
+    @media only screen and (min-width: 1024px) {
+        .woerterbuch__suche--input {
+            /*width: 70%;*/
+            width: 100%;
+        }
+    }
 
 
 </style>
